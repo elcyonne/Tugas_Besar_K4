50509df9e1883fca7c8d410d1d1c7ac8ab2e45c1
Ganesha Rahman Gibran <ganeshagibran4@gmail.com>
2023-06-20 21:27:39 +0700

    Workspace Gibran

--- prog-2.py
+++ prog-2.py
@@ -1,155 +1,139 @@
-#Workspace - Gibranr try error
-menu = {
-    "Laptop": [],
-    "Keyboard": [],
-    "Mouse": [],
-    "Monitor": [],
-    "RAM": [],
-    "Graphic Card": [],
-    "Processor": [],
-    "Kembali": []
+komputer = {
+    "Laptop": [
+        ("Lenovo ThinkPad", 10000000, "Intel Core i5, 8GB RAM, 256GB SSD"),
+        ("Dell XPS", 15000000, "Intel Core i7, 16GB RAM, 512GB SSD"),
+        ("HP Spectre", 12000000, "Intel Core i5, 8GB RAM, 256GB SSD")
+    ],
+    "PC": [
+        ("HP Pavilion", 8000000, "Intel Core i5, 8GB RAM, 1TB HDD"),
+        ("Asus ROG", 12000000, "Intel Core i7, 16GB RAM, 1TB SSD"),
+        ("Acer Predator", 10000000, "Intel Core i7, 16GB RAM, 512GB SSD")
+    ]
 }
 
-keranjang = []
+def tambah_produk(kategori):
+    nama_produk = input("Masukkan Nama Produk: ")
+    harga = int(input("Masukkan Harga: "))
+    spesifikasi = input("Masukkan Spesifikasi: ")
+    komputer[kategori].append((nama_produk, harga, spesifikasi))
+    print("Produk berhasil ditambahkan.")
 
-def tampilkan_daftar_menu(kategori):
-    if len(menu[kategori]) == 0:
-        print(f"Daftar Menu {kategori} kosong.")
+def hapus_produk(kategori):
+    tampilkan_daftar_produk(kategori)
+    nomor_produk = int(input("Masukkan Nomor Produk yang akan dihapus: "))
+    if nomor_produk > 0 and nomor_produk <= len(komputer[kategori]):
+        produk_terhapus = komputer[kategori].pop(nomor_produk - 1)
+        print(f"Produk '{produk_terhapus[0]}' berhasil dihapus.")
     else:
-        print(f"Daftar Menu {kategori}:")
-        print("-----------------------------------------")
-        print("| No. | Nama Produk  | Harga | Spesifikasi |")
-        print("-----------------------------------------")
-        for i, produk in enumerate(menu[kategori], start=1):
-            nama_produk, harga, spesifikasi = produk
-            print(f"| {i:3} | {nama_produk:12} | {harga:5} | {spesifikasi:11} |")
-        print("-----------------------------------------")
+        print("Nomor produk tidak valid.")
 
-def pilih_menu(kategori):
+def tampilkan_daftar_produk(kategori):
+    print(f"Daftar {kategori}:")
+    for i, produk in enumerate(komputer[kategori], start=1):
+        nama_produk, harga, spesifikasi = produk
+        print(f"{i}. {nama_produk} - Rp {harga} - {spesifikasi}")
+
+def pilih_produk(kategori):
+    tampilkan_daftar_produk(kategori)
     nomor_produk = int(input("Masukkan Nomor Produk yang dipilih: "))
-    if nomor_produk > 0 and nomor_produk <= len(menu[kategori]):
+    if nomor_produk > 0 and nomor_produk <= len(komputer[kategori]):
+        produk_terpilih = komputer[kategori][nomor_produk - 1]
         jumlah = int(input("Masukkan Jumlah: "))
-        catatan = input("Masukkan Catatan: ")
-        produk = menu[kategori][nomor_produk - 1]
-        return [(produk, jumlah, catatan)]
+        catatan = input("Masukkan Catatan (opsional): ")
+        return [(produk_terpilih, jumlah, catatan)]
     else:
         print("Nomor produk tidak valid.")
         return []
 
-def filter_menu(kategori):
-    filter_harga = int(input("Masukkan Harga Maksimum: "))
-    produk_terfilter = [produk for produk in menu[kategori] if produk[1] <= filter_harga]
+def filter_produk(kategori):
+    harga_min = int(input("Masukkan Harga Minimum: "))
+    harga_max = int(input("Masukkan Harga Maksimum: "))
+    produk_terfilter = [produk for produk in komputer[kategori] if harga_min <= produk[1] <= harga_max]
     if len(produk_terfilter) == 0:
-        print(f"Tidak ada produk {kategori} dengan harga di bawah {filter_harga}.")
+        print(f"Tidak ada {kategori} dalam rentang harga tersebut.")
     else:
-        print(f"Daftar Menu {kategori} (Harga di bawah {filter_harga}):")
-        print("-----------------------------------------")
-        print("| No. | Nama Produk  | Harga | Spesifikasi |")
-        print("-----------------------------------------")
+        print(f"Daftar {kategori} (Harga: {harga_min} - {harga_max}):")
         for i, produk in enumerate(produk_terfilter, start=1):
             nama_produk, harga, spesifikasi = produk
-            print(f"| {i:3} | {nama_produk:12} | {harga:5} | {spesifikasi:11} |")
-        print("-----------------------------------------")
+            print(f"{i}. {nama_produk} - Rp {harga} - {spesifikasi}")
 
-def cari_menu(kategori):
+def cari_produk(kategori):
     kata_kunci = input("Masukkan Kata Kunci: ")
-    produk_terfilter = [produk for produk in menu[kategori] if kata_kunci.lower() in produk[0].lower()]
+    produk_terfilter = [produk for produk in komputer[kategori] if kata_kunci.lower() in produk[0].lower()]
     if len(produk_terfilter) == 0:
-        print(f"Tidak ada produk {kategori} yang cocok dengan kata kunci '{kata_kunci}'.")
+        print(f"Tidak ada {kategori} yang cocok dengan kata kunci '{kata_kunci}'.")
     else:
-        print(f"Daftar Menu {kategori} (Pencarian: '{kata_kunci}'):")
-        print("-----------------------------------------")
-        print("| No. | Nama Produk  | Harga | Spesifikasi |")
-        print("-----------------------------------------")
+        print(f"Daftar {kategori} (Pencarian: '{kata_kunci}'):")
         for i, produk in enumerate(produk_terfilter, start=1):
             nama_produk, harga, spesifikasi = produk
-            print(f"| {i:3} | {nama_produk:12} | {harga:5} | {spesifikasi:11} |")
-        print("-----------------------------------------")
+            print(f"{i}. {nama_produk} - Rp {harga} - {spesifikasi}")
 
 def tampilkan_keranjang():
     if len(keranjang) == 0:
         print("Keranjang belanja kosong.")
     else:
         print("Rangkuman Keranjang Belanja:")
-        print("---------------------------------------------")
-        print("| No. | Nama Produk  | Jumlah | Catatan | Harga |")
-        print("---------------------------------------------")
         total_harga = 0
         for i, (produk, jumlah, catatan) in enumerate(keranjang, start=1):
             nama_produk, harga, _ = produk
             subtotal = harga * jumlah
             total_harga += subtotal
-            print(f"| {i:3} | {nama_produk:12} | {jumlah:6} | {catatan:7} | {subtotal:5} |")
-        print("---------------------------------------------")
-        print(f"Total Harga: {total_harga}")
+            print(f"{i}. {nama_produk} - {jumlah} - {catatan} - Rp {subtotal}")
+        print(f"Total Harga: Rp {total_harga}")
 
 def checkout():
     if len(keranjang) == 0:
         print("Keranjang belanja kosong. Tidak ada yang bisa di-checkout.")
     else:
         tampilkan_keranjang()
-        print("---------------------------------------------")
         total_harga = sum(harga * jumlah for produk, jumlah, _ in keranjang)
         print("Total yang harus dibayar:", total_harga)
-        print("---------------------------------------------")
-        konfirmasi = input("Apakah Anda ingin melakukan pembayaran? (y/n): ")
-        if konfirmasi.lower() == "y":
-            print("Pembayaran berhasil.")
+        konfirmasi = input("Apakah Anda yakin ingin melakukan checkout? (ya/tidak) ")
+        if konfirmasi.lower() == "ya":
+            print("Checkout berhasil. Terima kasih telah berbelanja!")
             keranjang.clear()
         else:
-            print("Pembayaran dibatalkan.")
+            print("Checkout dibatalkan.")
 
-def tambah_produk(kategori):
-    nama_produk = input("Masukkan Nama Produk: ")
-    harga = int(input("Masukkan Harga: "))
-    spesifikasi = input("Masukkan Spesifikasi: ")
-    produk = (nama_produk, harga, spesifikasi)
-    menu[kategori].append(produk)
-    print(f"Produk '{nama_produk}' telah ditambahkan ke menu {kategori}.")
+def tampilkan_menu_penjual():
+    username = input("Masukkan Username: ")
+    password = input("Masukkan Password: ")
 
-def hapus_produk(kategori):
-    tampilkan_daftar_menu(kategori)
-    nomor_produk = int(input("Masukkan Nomor Produk yang ingin dihapus: "))
-    if nomor_produk > 0 and nomor_produk <= len(menu[kategori]):
-        nama_produk, _, _ = menu[kategori][nomor_produk - 1]
-        del menu[kategori][nomor_produk - 1]
-        print(f"Produk '{nama_produk}' telah dihapus dari menu {kategori}.")
+    if username == "admin" and password == "admin":
+        while True:
+            print("=== Menu Penjual ===")
+            print("1. Tampilkan Daftar Produk")
+            print("2. Tambah Produk")
+            print("3. Hapus Produk")
+            print("4. Kembali")
+            pilihan_menu = input("Masukkan Pilihan: ")
+
+            if pilihan_menu == "1":
+                for kategori in komputer:
+                    tampilkan_daftar_produk(kategori)
+            elif pilihan_menu == "2":
+                kategori = input("Masukkan Kategori Produk: ")
+                if kategori in komputer:
+                    tambah_produk(kategori)
+                else:
+                    print("Kategori produk tidak valid.")
+            elif pilihan_menu == "3":
+                kategori = input("Masukkan Kategori Produk: ")
+                if kategori in komputer:
+                    hapus_produk(kategori)
+                else:
+                    print("Kategori produk tidak valid.")
+            elif pilihan_menu == "4":
+                return
+            else:
+                print("Pilihan tidak valid!")
     else:
-        print("Nomor produk tidak valid.")
+        print("Username atau password salah.")
 
-def tampilkan_menu_seller():
-    while True:
-        print("=== Menu Penjual ===")
-        print("1. Tampilkan Daftar Menu")
-        print("2. Tambah Produk")
-        print("3. Hapus Produk")
-        print("4. Kembali")
-        pilihan_menu = input("Masukkan Pilihan: ")
-
-        if pilihan_menu == "1":
-            for kategori in menu:
-                tampilkan_daftar_menu(kategori)
-        elif pilihan_menu == "2":
-            kategori = input("Masukkan Kategori Produk: ")
-            if kategori in menu:
-                tambah_produk(kategori)
-            else:
-                print("Kategori produk tidak valid.")
-        elif pilihan_menu == "3":
-            kategori = input("Masukkan Kategori Produk: ")
-            if kategori in menu:
-                hapus_produk(kategori)
-            else:
-                print("Kategori produk tidak valid.")
-        elif pilihan_menu == "4":
-            return
-        else:
-            print("Pilihan tidak valid!")
-
-def tampilkan_menu_buyer():
+def tampilkan_menu_pembeli():
     while True:
         print("=== Menu Pembeli ===")
-        print("1. Tampilkan Daftar Menu")
+        print("1. Tampilkan Daftar Produk")
         print("2. Pilih Produk")
         print("3. Filter Produk Berdasarkan Harga")
         print("4. Cari Produk")
@@ -159,25 +143,26 @@ def tampilkan_menu_buyer():
         pilihan_menu = input("Masukkan Pilihan: ")
 
         if pilihan_menu == "1":
-            for kategori in menu:
-                tampilkan_daftar_menu(kategori)
+            for kategori in komputer:
+                tampilkan_daftar_produk(kategori)
         elif pilihan_menu == "2":
             kategori = input("Masukkan Kategori Produk: ")
-            if kategori in menu:
-                produk_terpilih = pilih_menu(kategori)
-                keranjang.extend(produk_terpilih)
+            if kategori in komputer:
+                produk_terpilih = pilih_produk(kategori)
+                if len(produk_terpilih) > 0:
+                    keranjang.extend(produk_terpilih)
             else:
                 print("Kategori produk tidak valid.")
         elif pilihan_menu == "3":
             kategori = input("Masukkan Kategori Produk: ")
-            if kategori in menu:
-                filter_menu(kategori)
+            if kategori in komputer:
+                filter_produk(kategori)
             else:
                 print("Kategori produk tidak valid.")
         elif pilihan_menu == "4":
             kategori = input("Masukkan Kategori Produk: ")
-            if kategori in menu:
-                cari_menu(kategori)
+            if kategori in komputer:
+                cari_produk(kategori)
             else:
                 print("Kategori produk tidak valid.")
         elif pilihan_menu == "5":
@@ -189,22 +174,20 @@ def tampilkan_menu_buyer():
         else:
             print("Pilihan tidak valid!")
 
-def main_menu():
-    while True:
-        print("=== Aplikasi Penjualan ===")
-        print("1. Menu Penjual")
-        print("2. Menu Pembeli")
-        print("3. Keluar")
-        pilihan_menu = input("Masukkan Pilihan: ")
+keranjang = []
 
-        if pilihan_menu == "1":
-            tampilkan_menu_seller()
-        elif pilihan_menu == "2":
-            tampilkan_menu_buyer()
-        elif pilihan_menu == "3":
-            return
-        else:
-            print("Pilihan tidak valid!")
+while True:
+    print("=== Aplikasi Jual Beli Komputer ===")
+    print("1. Menu Penjual")
+    print("2. Menu Pembeli")
+    print("3. Keluar")
+    pilihan_menu = input("Masukkan Pilihan: ")
 
-main_menu()
-
+    if pilihan_menu == "1":
+        tampilkan_menu_penjual()
+    elif pilihan_menu == "2":
+        tampilkan_menu_pembeli()
+    elif pilihan_menu == "3":
+        break
+    else:
+        print("Pilihan tidak valid!")
